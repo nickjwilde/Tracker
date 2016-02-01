583e0022f8bd58056d407b8c6fed5c311cb7f4dd
rcwoolston <rcwoolston@hotmail.com>
2016-02-01 15:28:28 -0700

    Create Builder.rb

---
+++ Tracker/Builder.rb
@@ -0,0 +1,33 @@
+class Builder
+  
+  def initialize(id="-1",name="empty",phone="-1")
+    @builder_id = id
+    @name_of_builder= name
+    @phone_number = phone
+  end
+
+  def set_builder_id(id)
+    @builder_id = id
+  end
+  #
+  def get_builder_id()
+    @builder_id
+  end
+
+  def set_name_of_builder(name)
+    @name_of_builder = name
+  end
+
+  def get_name_of_builder()
+    @name_of_builder
+  end
+
+  def set_phone_number(phone)
+    @phone_number = phone
+  end
+
+  def get_phone_number()
+    @phone_number
+  end
+
+end
