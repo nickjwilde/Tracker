510435213d3d036688be214b7ad39d7ab4b02ded
nickjwilde <nickjwilde@gmail.com>
2016-03-20 04:54:09 +0000

    Enable Google Authentication

--- Tracker/app/assets/javascripts/ui.js
+++ Tracker/app/assets/javascripts/ui.js
@@ -1,14 +1,24 @@
 $(document).ready(function(){
-	$('.entry').click(function(){
+	$('#event-column .entry').click(function(){
 		if($(this).hasClass("selected")){
 			$(this).removeClass("selected");
-			$('.entry').not(this).slideDown(1000);
-			$(this).parent().siblings().children('.column-header').slideDown();
+			$('#event-column .column-header').fadeTo(1000,1);
+			$('#event-column .entry').not(this).slideDown(1000);
 		}else{
 			$(this).addClass("selected");
-			$('.entry').not(this).slideUp(1000);
-			$(this).parent().siblings().children('.column-header').slideUp();
-			$('#home-column').removeClass('hidden');
+			$('#event-column .column-header').fadeTo(1000,0);
+			$('#event-column .entry').not(this).slideUp(1000);
+		}
+  });
+	$('#home-column .entry').click(function(){
+		if($(this).hasClass("selected")){
+			$(this).removeClass("selected");
+			$('#home-column .column-header').slideDown(1000);
+			$('#home-column .entry').not(this).slideDown(1000);
+		}else{
+			$(this).addClass("selected");
+			$('#home-column .column-header').slideUp(1000);
+			$('#home-column .entry').not(this).slideUp(1000);
 		}
 	});
 });
--- Tracker/config/routes.rb
+++ Tracker/config/routes.rb
@@ -3,7 +3,14 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-root 'admin#home'
+  get 'auth/:provider/callback', to: 'sessions#create'
+  get 'auth/failure', to: redirect('/')
+  get 'signout', to: 'sessions#destroy', as: 'signout'
+
+  resources :sessions, only: [:create, :destroy]
+  resource :home, only: [:show]
+
+  root 'admin#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
