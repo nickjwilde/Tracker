94c634e65e57320c00ddb6e886cfafa8a0c3b7f7
nickjwilde <nickjwilde@gmail.com>
2016-03-21 18:15:14 +0000

    Enable Google Oauth2 - fix minor code errors

--- Tracker/app/controllers/admin_controller.rb
+++ Tracker/app/controllers/admin_controller.rb
@@ -1,7 +1,7 @@
 class AdminController < ApplicationController
   def home
     worker = Factory.new
-    worker.connect_to_db("")
+    worker.connect_to_db("nitrous","")
     @results = worker.interact_with_parade('L')
   end
 end
--- Tracker/db/schema.rb
+++ Tracker/db/schema.rb
@@ -11,9 +11,66 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160321012430) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "builder", primary_key: "builder_id", force: true do |t|
+    t.string   "name_of_builder", limit: 100,                   null: false
+    t.string   "phone_number",    limit: 14
+    t.string   "email_address",   limit: 100
+    t.datetime "time_stamp",                  default: "now()", null: false
+  end
+
+  create_table "home", primary_key: "home_id", force: true do |t|
+    t.string   "home_name",    limit: 100
+    t.string   "home_address", limit: 100
+    t.string   "city",         limit: 100
+    t.string   "state",        limit: 100
+    t.string   "notes",        limit: 10000
+    t.integer  "parade_id"
+    t.integer  "builder_id"
+    t.datetime "time_stamp",                 default: "now()", null: false
+  end
+
+  create_table "package", primary_key: "package_id", force: true do |t|
+    t.integer  "number_of_photos"
+    t.string   "notes_of_package", limit: 10000
+    t.datetime "time_stamp",                     default: "now()", null: false
+  end
+
+  create_table "parade", primary_key: "parade_id", force: true do |t|
+    t.string   "name_of_parade",       limit: 1000,                    null: false
+    t.string   "city_of_parade",       limit: 100
+    t.string   "state_of_parade",      limit: 100
+    t.datetime "start_date_of_parade"
+    t.datetime "end_date_of_parade"
+    t.string   "parade_notes",         limit: 10000
+    t.datetime "time_stamp",                         default: "now()", null: false
+  end
+
+  create_table "photographer", primary_key: "photographer_id", force: true do |t|
+    t.string   "name_of_photographer",         limit: 100,                    null: false
+    t.string   "email_of_photographer",        limit: 100
+    t.string   "phone_number_of_photographer", limit: 14
+    t.string   "notes_of_photographer",        limit: 1000
+    t.datetime "time_stamp",                                default: "now()", null: false
+  end
+
+  create_table "test", id: false, force: true do |t|
+    t.integer "id",                null: false
+    t.string  "name", limit: 1000
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "name"
+    t.string   "oauth_token"
+    t.datetime "oauth_expires_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
